23dbff1ce7a10d7eddd79ca8aeb8a3c85763f5ea
智能大石头 <nnhy@vip.qq.com>
2022-08-10 08:07:43 +0800

    IP组件独立

---
+++ XUnitTest/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NewLife;
+using NewLife.Caching;
+using NewLife.Log;
+using Xunit;
+
+namespace XUnitTest;
+
+public class BasicTest
+{
+   
+}
