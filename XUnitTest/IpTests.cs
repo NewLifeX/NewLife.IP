bc2552d675ba2c3541a7e9425b9446d777d5baeb
大石头 <nnhy@vip.qq.com>
2023-02-03 22:14:13 +0800

    v2.0.2023.0203

--- XUnitTest/IpTests.cs
+++ XUnitTest/IpTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using NewLife;
 using NewLife.IP;
 using Xunit;
@@ -13,7 +9,7 @@ public class IpTests
 {
     static IpTests()
     {
-        //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         IpResolver.Register();
     }
 
