87ea9bfbe296b93d79ff72b503627bf28a59da96
大石头 <nnhy@vip.qq.com>
2024-08-05 17:02:28 +0800

    拦截日志库解析异常，不要影响业务

--- NewLife.IP/IpResolver.cs
+++ NewLife.IP/IpResolver.cs
@@ -11,7 +11,17 @@ public class IpResolver : IIPResolver
     /// <summary>获取物理地址</summary>
     /// <param name="addr"></param>
     /// <returns></returns>
-    public String GetAddress(IPAddress addr) => _ip.GetAddress(addr);
+    public String GetAddress(IPAddress addr)
+    {
+        try
+        {
+            return _ip.GetAddress(addr);
+        }
+        catch
+        {
+            return String.Empty;
+        }
+    }
 
     /// <summary>注册IP地址解析器</summary>
     public static void Register()
