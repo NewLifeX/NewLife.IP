1d217cd0afaebc5551fa4a141f8b316fb548eb58
Aven <Aven@ocr.one>
2024-08-07 10:57:16 +0800

    修正单元测试

--- XUnitTest/IpTests.cs
+++ XUnitTest/IpTests.cs
@@ -26,43 +26,43 @@ public class IpTests
     {
         var addr = "39.144.10.35".IPToAddress();
         var ss = addr.Split(' ');
-        Assert.Equal("广东省", ss[0]);
+        Assert.Equal("中国–广东", ss[0]);
 
         addr = "116.234.91.199".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("上海市", ss[0]);
+        Assert.Equal("中国–上海–上海", ss[0]);
 
         addr = "61.160.219.25".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("江苏省常州市武进区", ss[0]);
+        Assert.Equal("中国–江苏–常州–武进区", ss[0]);
 
         addr = "123.14.85.208".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("河南省郑州市", ss[0]);
+        Assert.Equal("中国–河南–郑州", ss[0]);
 
         addr = "113.220.60.29".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("湖南省邵阳市", ss[0]);
+        Assert.Equal("中国–湖南–邵阳", ss[0]);
 
         addr = "124.239.170.77".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("河北省衡水市", ss[0]);
+        Assert.Equal("中国–河北–衡水", ss[0]);
 
         addr = "112.74.79.65".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("广东省深圳市", ss[0]);
+        Assert.Equal("中国–广东–深圳", ss[0]);
 
         addr = "218.87.90.59".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("江西省九江市永修县", ss[0]);
+        Assert.Equal("中国–江西–九江–永修县", ss[0]);
 
         addr = "39.144.8.87".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("广东省深圳市", ss[0]);
+        Assert.Equal("中国–广东–深圳", ss[0]);
 
         addr = "111.55.141.170".IPToAddress();
         ss = addr.Split(' ');
-        Assert.Equal("山西省", ss[0]);
+        Assert.Equal("中国–山西", ss[0]);
     }
 
     [Fact]
@@ -70,7 +70,7 @@ public class IpTests
     {
         var addr = "116.136.7.43".IPToAddress();
         var ss = addr.Split(' ');
-        Assert.Equal("内蒙古赤峰市", ss[0]);
+        Assert.Equal("中国–内蒙古–赤峰", ss[0]);
         Assert.Equal("", ss[1]);
     }
 }
